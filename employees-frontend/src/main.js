d06db84771939a8473af317e3c3eb8fdf06364eb
Shathiso <shathisontibi@yahoo.com>
2024-03-04 14:41:23 +0200

    Configured Tailwind, Quasar and Vite

--- employees-frontend/src/main.js
+++ employees-frontend/src/main.js
@@ -1,14 +1,49 @@
 import './assets/main.css'
 
+import { Quasar, Dialog } from "quasar";
+import piniaPersist from 'pinia-plugin-persist'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Store } from './stores/index.js';
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+// Vue Axios
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// import icon library
+import '@quasar/extras/material-icons/material-icons.css';
+// import Quasar css
+import 'quasar/src/css/index.sass';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+
+//This allows pinia to use local storage
+pinia.use(piniaPersist)
+
+app.use(Toast)
+app.use(pinia)
 app.use(router)
+
+app.use(Quasar, {
+    plugins: {
+        Dialog
+    }
+});
+
+
+// Provide Pinia Store globally
+app.provide('store', Store);
+
+// Use vue-axios
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)  // provide 'axios'
 
 app.mount('#app')
---
+++ employees-frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  important: true,
+  content: [
+    './index.html',
+    './src/**/*.vue',
+    './src/**/*.js',
+  ],
+  darkMode:'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    
+  ],
+}
+
